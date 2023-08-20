b218272c9c3018ceb56fa077d9f218ff50a64f8f
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-08-20 14:45:08 -0400

    adds medium field to craft model

--- eno/admin.py
+++ eno/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User, Participant, Craft
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Participant)
+admin.site.register(Craft)
--- eno/models.py
+++ eno/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+MEDIUM_CHOICES = (
+    ('clay', 'ceramics'),
+    ('wood', 'wood'),
+    ('paint', 'paint'),
+    ('drawing', 'drawing'),
+    ('textile', 'textile'),
+)
 
 
 class User(AbstractUser):
@@ -13,15 +20,18 @@ class Participant(models.Model):
     business_name = models.CharField(max_length=150)
     contact_name = models.CharField(max_length=250)
     email = models.EmailField()
-    phone = models.models.PhoneNumberField(_(""))
+    phone = PhoneNumberField(region='US')
     address_street = models.CharField(max_length=300)
     address_city = models.CharField(max_length=300)
     address_state = models.CharField(max_length=2)
-    address_zip = models.IntegerField(max_length=5)
-    bio = models.TextField()
+    address_zip = models.IntegerField()
+    bio = models.TextField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
-    booth_number = models.IntegerField(max_length=999)
+    booth_number = models.IntegerField()
 
 
 class Craft(Participant):
-    pass
+    medium = models.CharField(choices=MEDIUM_CHOICES)
+
+    def __str__(self):
+        return self.business_name
